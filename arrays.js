3d55004485f9bf79bb8c3881d8e8caac66fafd8d
Matt Walter <matt.walter.tx@outlook.com>
2022-06-08 11:25:42 -0500

    add array methods

--- arrays.js
+++ arrays.js
@@ -2,4 +2,26 @@ var names = ["Matt", "Kerri", "Elizabeth", "Evelyn", "Liam"];
 
 var nums = [1, 2, 3, 4]
 
-var boos = [0, null, true, false];
+var boos = [false, true, true, false];
+
+var count = names.push("Walter");
+
+console.log(names);
+// expected output: Array ["Matt", "Kerri", "Elizabeth", "Evelyn", "Liam", "walter"]
+
+
+
+console.log(nums.pop());
+// expected output: 4
+
+console.log(nums);
+// expected output: Array [1, 2, 3]
+
+
+var firstElement = boos.shift();
+
+console.log(boos);
+// expected output: [true, true, false]
+
+console.log(firstElement);
+// expected output: false
